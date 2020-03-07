eb3e15427a64ade91864353af65773bc9ebf8525
fang <yadasanda@gmail.com>
2020-03-07 12:24:15 -0800

    Updates

---
+++ precache-manifest.f49a87f696dec003e5d422ebb8070949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebb133c928c280a2cfdecc7083b6cf7",
+    "url": "/DJVanCounter/index.html"
+  },
+  {
+    "revision": "b96bbfd10fdd7b5c2f1c",
+    "url": "/DJVanCounter/static/css/main.7561bac2.chunk.css"
+  },
+  {
+    "revision": "ea0211a4587cb0ef1a95",
+    "url": "/DJVanCounter/static/js/2.12916041.chunk.js"
+  },
+  {
+    "revision": "48a7f982264b141da44db572a26ecb94",
+    "url": "/DJVanCounter/static/js/2.12916041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96bbfd10fdd7b5c2f1c",
+    "url": "/DJVanCounter/static/js/main.8ff8c01e.chunk.js"
+  },
+  {
+    "revision": "f4c71d591066776f66c8",
+    "url": "/DJVanCounter/static/js/runtime-main.9b25d053.js"
+  },
+  {
+    "revision": "2ce30af658e226be1d10c3c25bd50681",
+    "url": "/DJVanCounter/static/media/bbe3559b-3585-4c7a-91ba-10706bd81a53.2ce30af6.jpeg"
+  }
+]);
