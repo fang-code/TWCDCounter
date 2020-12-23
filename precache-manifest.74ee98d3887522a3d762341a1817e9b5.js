25ba4886e2b09951738c6c6fc7e559c785343101
fang-code <yadasanda@gmail.com>
2020-12-22 16:40:40 -0800

    Updates

---
+++ precache-manifest.74ee98d3887522a3d762341a1817e9b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42721a265c7ac4560a0ac9266cd5777f",
+    "url": "/TWCDCounter/index.html"
+  },
+  {
+    "revision": "176f9730405d4b8e7f6e",
+    "url": "/TWCDCounter/static/css/main.8a889c61.chunk.css"
+  },
+  {
+    "revision": "363f1478bae8312b2e18",
+    "url": "/TWCDCounter/static/js/2.4f7baee1.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/TWCDCounter/static/js/2.4f7baee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176f9730405d4b8e7f6e",
+    "url": "/TWCDCounter/static/js/main.9fdad8a8.chunk.js"
+  },
+  {
+    "revision": "7e4be2ab03c85addcea4",
+    "url": "/TWCDCounter/static/js/runtime-main.9decfa7b.js"
+  },
+  {
+    "revision": "2ce30af658e226be1d10c3c25bd50681",
+    "url": "/TWCDCounter/static/media/bbe3559b-3585-4c7a-91ba-10706bd81a53.2ce30af6.jpeg"
+  }
+]);
